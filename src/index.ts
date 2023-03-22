77d3b0625be23ad69860da97c21b145526cce4e6
Rodrigo Tavares <86687715+Rodr1goTavares@users.noreply.github.com>
2023-03-21 23:39:36 -0300

    .

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import { client } from "./config/Client";
+
+
+client().on('ready', () => {
+    console.log("(((Rbot is online)))");
+
+    //Funcionalidades
+})
+
+async function exemple(){
+    
+}
--- src/modules/SlashCommands/commands.ts
+++
@@ -1,8 +0,0 @@
-import { client } from '../Client';
-import { CommandInteraction, IntegrationExpireBehavior, SlashCommandBuilder } from 'discord.js';
-
-export class Commands{
-    public static listenChat(): void{
-        
-    }
-}
